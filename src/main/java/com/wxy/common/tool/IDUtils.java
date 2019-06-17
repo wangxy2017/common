3e03807832ac3694481b48a4dd25369dcd530267
王孝元 <wangxiaoyuan@pand-auto.com>
2019-06-17 16:04:05 +0800

    新增IDUtils

---
+++ src/main/java/com/wxy/common/tool/IDUtils.java
@@ -0,0 +1,31 @@
+package com.wxy.common.tool;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * @Author wangxy
+ * @Date 2019/6/17 15:35
+ * @Description ID生成工具
+ **/
+public class IDUtils {
+    /**
+     * 获取 UUID
+     *
+     * @return
+     */
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
+     * 生成图片名
+     *
+     * @return
+     */
+    public static String genImageName() {
+        return System.currentTimeMillis() + String.format("%03d", new Random().nextInt(999));
+    }
+}
