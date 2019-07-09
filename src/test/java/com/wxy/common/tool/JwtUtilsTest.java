dd79db2e0a32d9d6f8755c5d79ef3ad17741b278
wxy <wangxiaoyuan@pand-auto.com>
2019-07-09 15:47:02 +0800

    jwt工具类

---
+++ src/test/java/com/wxy/common/tool/JwtUtilsTest.java
@@ -0,0 +1,48 @@
+package com.wxy.common.tool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @Author wxy
+ * @Date 19-7-9 下午3:13
+ * @Description TODO
+ **/
+@Slf4j
+public class JwtUtilsTest {
+
+    @Test
+    public void TestSign() {
+        String username = "admin";
+        String userId = "1";
+        String token = JwtUtils.sign(username, userId);
+        log.info("生成token = {}", token);
+    }
+
+    @Test
+    public void TestVerity() {
+        String username = "admin";
+        String userId = "1";
+        String token = JwtUtils.sign(username, userId);
+        boolean verity = JwtUtils.verity(token);
+        log.info("验证token = {}", verity);
+    }
+
+    @Test
+    public void TestGetUsername() {
+        String username = "admin";
+        String userId = "1";
+        String token = JwtUtils.sign(username, userId);
+        String username1 = JwtUtils.getUsername(token);
+        log.info("获取用户名 username = {}", username1);
+    }
+
+    @Test
+    public void TestGetUserId() {
+        String username = "admin";
+        String userId = "1";
+        String token = JwtUtils.sign(username, userId);
+        String userId1 = JwtUtils.getUserId(token);
+        log.info("获取用户ID：userId = {}", userId1);
+    }
+}
