d5a057552cf8174fc78326d1b53f6656118d098d
wxy <wangxiaoyuan@pand-auto.com>
2019-09-16 16:08:21 +0800

    RES

--- src/main/java/com/wxy/common/response/ApiResponse.java
+++ src/main/java/com/wxy/common/response/ApiResponse.java
@@ -13,25 +13,27 @@ public class ApiResponse {
     private String msg;
     private Object data;
 
-    public ApiResponse(Integer code, String msg, Object data) {
+    private ApiResponse(Integer code, String msg, Object data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    private static final Integer ERROR_CODE = 0;
+    private static final Integer ERROR_CODE = -1;
+    private static final String ERROR_MSG = "ERROR";
     private static final Integer SUCCESS_CODE = 1;
+    private static final String SUCCESS_MSG = "SUCCESS";
 
     public static ApiResponse success() {
-        return new ApiResponse(SUCCESS_CODE, "success", null);
+        return new ApiResponse(SUCCESS_CODE, SUCCESS_MSG, null);
     }
 
     public static ApiResponse success(Object data) {
-        return new ApiResponse(SUCCESS_CODE, "success", data);
+        return new ApiResponse(SUCCESS_CODE, SUCCESS_MSG, data);
     }
 
     public static ApiResponse error() {
-        return new ApiResponse(ERROR_CODE, "error", null);
+        return new ApiResponse(ERROR_CODE, ERROR_MSG, null);
     }
 
     public static ApiResponse error(Integer code, String msg) {
