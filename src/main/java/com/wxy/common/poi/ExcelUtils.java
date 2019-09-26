feae8792b08246facf252193b2c1d594284be2f2
wxy <wangxiaoyuan@pand-auto.com>
2019-09-26 11:06:24 +0800

    excel

--- src/main/java/com/wxy/common/poi/ExcelUtils.java
+++ src/main/java/com/wxy/common/poi/ExcelUtils.java
@@ -27,32 +27,28 @@ public class ExcelUtils {
      * @param values
      * @return
      */
-    public static HSSFWorkbook getHSSFWorkbook(String sheetName,
-                                               String[] title, String[][] values) {
+    public static HSSFWorkbook buildExcel(HSSFWorkbook workbook, String sheetName,
+                                          String[] title, String[][] values) {
         // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
-        HSSFWorkbook workbook = new HSSFWorkbook();
-
+        if (workbook == null) {
+            workbook = new HSSFWorkbook();
+        }
         // 第二步，在workbook中添加一个sheet，对应Excel文件中的sheet
         HSSFSheet sheet = workbook.createSheet(sheetName);
-
         // 第三步，在sheet中添加表头第0行，注意老版本poi对Excel行数列数有限制
         HSSFRow row = sheet.createRow(0);
-
         // 第四步，创建单元格，并设置表头 设置表头居中
         HSSFCellStyle style = workbook.createCellStyle();
         style.setAlignment(HorizontalAlignment.CENTER);// 水平居中
         style.setVerticalAlignment(VerticalAlignment.CENTER);// 垂直居中
-
         // 声明列对象
         HSSFCell cell = null;
-
         // 创建标题
         for (int i = 0; i < title.length; i++) {
             cell = row.createCell(i);
             cell.setCellValue(title[i]);
             cell.setCellStyle(style);
         }
-
         // 创建内容
         for (int i = 0; i < values.length; i++) {
             row = sheet.createRow(i + 1);
@@ -77,7 +73,8 @@ public class ExcelUtils {
     public static void downloadExcel(String fileName, String sheetName,
                                      String[] title, String[][] values, HttpServletResponse response) throws IOException {
         // 创建 HSSFWorkbook
-        HSSFWorkbook workbook = getHSSFWorkbook(sheetName, title, values);
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        buildExcel(workbook, sheetName, title, values);
         if (response != null) {// 下载
             // 设置响应头
             response.setContentType("application/vnd.ms-excel;charset=utf-8");
